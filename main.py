7c20dc42995d975f039cf8acbb18f6cb5291216f
Jin <134983017+Jin-Orgin@users.noreply.github.com>
2025-01-14 09:18:19 -0500

    upload

---
+++ main.py
@@ -0,0 +1,41 @@
+from model import *
+from plot import *
+from data import *
+
+import numpy as np
+import pandas as pd
+import questionary as qu
+
+def main():
+
+    #initialized start screen
+        #create/get account
+
+    #option and parameter
+        #symbol
+        #start date(scroll bar + hard input)
+        #label(bar, close, etc)
+
+    symbol = 'BTCUSD'
+
+
+    #=>async data(option, parameter)
+        #gather history
+        #stream live data
+
+    #=>async model(gather history, live data )
+        #process data
+        #train model
+        #return value(prediction) 
+
+    #=>plot(data() -> history) && (data -> live data == idel && model -> prediction)
+
+    #[automate buy and sell request]
+        #buget(account total)
+        #request(stock amt, price amt)
+        
+
+    return True
+if __name__ == "__main__":
+    main()
+
---
+++ model.py
@@ -0,0 +1,8 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+from keras._tf_keras.keras.models import Sequential
+from keras._tf_keras.keras.layers import Input, Dense, LSTM
+from keras._tf_keras.keras.optimizers import Adam
+from sklearn.preprocessing import MinMaxScaler
+from datetime import datetime
